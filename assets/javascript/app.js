61a1f69f884a418d4d689814bea07c2ce671bc0a
George <dog8dog1@live.com>
2019-07-10 16:50:14 -0400

    First version.  I will complete in the next few days

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// click to start then display quesions
+
+document.getElementById("start-btn").onclick = function() { 
+    document.getElementById("start-btn").style.display = "none";
+    document.getElementById("timer").style.display = "block";
+    document.getElementById("questions").style.display = "block"; 
+}; 
+  
+    
